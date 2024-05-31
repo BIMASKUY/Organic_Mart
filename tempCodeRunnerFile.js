3ce4f15c5258c5b3ff45b03ac609ebb6ee91f1e3
BIMASKUY <113542041+BIMASKUY@users.noreply.github.com>
2024-05-31 08:50:27 +0700

    Delete tempCodeRunnerFile.js

--- tempCodeRunnerFile.js
+++
@@ -1,7 +0,0 @@
-
-const username = 'admin'
-const password = '" union select 1 as id, "admin" as username, "pass" as password --'
-
-const query = 'SELECT * FROM users WHERE username = "' + username + '" and password = "' + password + '"'
-
-console.log(query)
